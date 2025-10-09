4824cc68072add78903c2cb89e1b8310d20c5edc
brynkinz2 <sweettewbme@gmail.com>
2025-10-09 09:00:06 -0600

    set up static files

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -7,7 +7,5 @@ public class Main {
         server.run(8080);
 
         System.out.println("♕ 240 Chess Server");
-//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-//        System.out.println("♕ 240 Chess Server: " + piece);
     }
 }

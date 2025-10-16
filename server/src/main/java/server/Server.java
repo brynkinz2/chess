ad76db7ddc093eafb7465641ce50901eea191276
brynkinz2 <sweettewbme@gmail.com>
2025-10-16 09:53:44 -0600

    Did some refractoring for how UserData is stored

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -6,20 +6,29 @@ import io.javalin.http.Context;
 import org.eclipse.jetty.server.Authentication;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Server {
 
     private final Javalin server;
-    private List<String> usernames = new ArrayList<>();
+    private Map<String, UserData> users = new HashMap<>();
 
     public Server() {
         server = Javalin.create(config -> config.staticFiles.add("web"));
 
         server.delete("db", ctx -> ctx.result("{}"));
         server.post("user", ctx -> register(ctx));
+        server.post("/session", ctx -> login(ctx));
+        server.delete("/session", ctx -> logout(ctx));
 
+    }
+
+    record UserData(
+            String username,
+            String password,
+            String authToken){
     }
 
     private void register(Context ctx) {
@@ -39,16 +48,55 @@ public class Server {
             ctx.result(serializer.toJson(errorRes));
             return;
         }
-        usernames.add(username);
+        UserData newUser = new UserData(username, password, "xyz");
+        users.put(username, newUser);
         var res = Map.of("username", username, "authToken", "xyz");
         ctx.result(serializer.toJson(res));
     }
 
     private boolean userAlreadyExists(String username) {
-        if (usernames.contains(username)) {
+        if (users.containsKey(username)) {
             return true;
         }
         return false;
+    }
+
+    private void login(Context ctx) {
+        var serializer = new Gson();
+        var req = serializer.fromJson(ctx.body(), Map.class);
+        String username = (String) req.get("username");
+        String password = (String) req.get("password");
+        if (username == null || password == null) {
+            ctx.status(400);
+            var errorRes = Map.of("message", "Error: Please include a username and password.");
+            ctx.result(serializer.toJson(errorRes));
+            return;
+        }
+        if (userAlreadyExists(username)) {
+            if (users.get(username).password.equals(password)) {
+                var res = Map.of("username", username, "authToken", "xyz");
+                ctx.result(serializer.toJson(res));
+                return;
+            }
+            else {
+                ctx.status(401);
+                var errorRes = Map.of("message", "Error: Username and password do not match!");
+                ctx.result(serializer.toJson(errorRes));
+                return;
+            }
+        }
+        else {
+            ctx.status(401);
+            var errorRes = Map.of("message", "Error: Username does not exist!");
+            ctx.result(serializer.toJson(errorRes));
+        }
+    }
+
+    private void logout(Context ctx) {
+        var serializer = new Gson();
+        var req = serializer.fromJson(ctx.body(), Map.class);
+        String username = (String) req.get("username");
+        return;
     }
 
     public int run(int desiredPort) {
@@ -59,4 +107,5 @@ public class Server {
     public void stop() {
         server.stop();
     }
+
 }

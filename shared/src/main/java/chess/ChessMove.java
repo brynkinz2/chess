1750e51264f99eab7921d64eec04ae7c7de5444d
brynkinz2 <sweettewbme@gmail.com>
2025-11-03 14:43:15 -0700

    fixed code quality in chessMove

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -62,9 +62,10 @@ public class ChessMove {
             return false;
         }
         ChessMove chessMove = (ChessMove) o;
-        return Objects.equals(startPosition, chessMove.startPosition) && Objects.equals(endPosition, chessMove.endPosition) && promotionPiece == chessMove.promotionPiece;
+        return (Objects.equals(startPosition, chessMove.startPosition) &&
+                Objects.equals(endPosition, chessMove.endPosition) &&
+                promotionPiece == chessMove.promotionPiece);
     }
-
     @Override
     public int hashCode() {
         return Objects.hash(startPosition, endPosition, promotionPiece);

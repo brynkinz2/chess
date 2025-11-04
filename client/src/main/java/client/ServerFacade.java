00d53fa15ff54ebca394a7720e022b87b4b3f7c0
brynkinz2 <sweettewbme@gmail.com>
2025-11-04 16:44:26 -0700

    first test (register) working!!!

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,68 @@
+package client;
+
+import com.google.gson.Gson;
+import model.UserData;
+import model.AuthData;
+import java.io.*;
+import java.net.*;
+
+
+
+public class ServerFacade {
+    private final String serverUrl;
+
+    public ServerFacade(int port) {
+        this.serverUrl = "http://localhost:" + port;
+    }
+
+    public AuthData register(String username, String password) throws IOException {
+        var request = new UserData(username, password);
+
+        return makeRequest("/user", "POST", request, AuthData.class);
+    }
+
+    public AuthData login(String username, String password) {
+        return null;
+    }
+
+    private <T> T makeRequest(String path, String method, Object request, Class<T> responseClass) throws IOException {
+        URL url = new URL(serverUrl + path);
+        HttpURLConnection connection =  (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod(method);
+        connection.setDoOutput(true);
+
+        // write the request body
+        if (request != null) {
+            connection.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream os = connection.getOutputStream()) {
+                os.write(reqData.getBytes());
+            }
+        }
+
+        // handle response
+        connection.connect();
+        int resCode = connection.getResponseCode();
+
+        if (resCode == 200) {
+            if (responseClass == null) {
+                return null;
+            }
+            //parse the response
+            try (InputStream is = connection.getInputStream()) {
+                InputStreamReader isr = new InputStreamReader(is);
+                return new Gson().fromJson(isr, responseClass);
+            }
+        } else {
+            try (InputStream is = connection.getErrorStream()) {
+                InputStreamReader isr = new InputStreamReader(is);
+                throw new IOException("Error: " + resCode);
+            }
+        }
+    }
+
+    public void clear() throws IOException {
+        makeRequest( "/db","DELETE", null, null);
+    }
+}
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,18 +2,23 @@ package client;
 
 import org.junit.jupiter.api.*;
 import server.Server;
+import model.*;
 import client.ServerFacade;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        serverFacade = new ServerFacade(port);
     }
 
     @AfterAll
@@ -21,10 +26,22 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    void clearDatabase() throws IOException {
+        serverFacade.clear();
+    }
+
 
     @Test
     public void sampleTest() {
         Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void registerSuccess() throws IOException {
+        var auth = serverFacade.register("user", "password");
+
+        assertNotNull(auth);
     }
 
 }

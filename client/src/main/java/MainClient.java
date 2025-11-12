2b691a4bf8af03cc86c4df01457e062c83b3dc3f
brynkinz2 <sweettewbme@gmail.com>
2025-11-12 16:14:18 -0700

    fixed setup of main

--- client/src/main/java/MainClient.java
+++ client/src/main/java/MainClient.java
@@ -1,19 +1,22 @@
 
-import server.Server;
 import ui.ChessClient;
 
 public class MainClient {
     public static void main(String[] args) {
-//        String serverUrl = "http://localhost:8080";
-        Server server = new Server();
-        var port = server.run(0);
-//        if (args.length == 1) {
-//            serverUrl = args[0];
-//        }
+        String serverUrl = "http://localhost:8080";
+        int port = 8080;
+        if (args.length == 1) {
+            serverUrl = args[0];
+            // Extract port from URL if provided
+            String[] parts = serverUrl.split(":");
+            if (parts.length == 3) {
+                port = Integer.parseInt(parts[2]);
+            }
+
+        }
 
         try {
             new ChessClient(port).run();
-
 
         } catch (Throwable ex) {
             System.out.printf("Unable to start server: %s%n", ex.getMessage());
